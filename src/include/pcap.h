e9364550b1ced9d12c0b0f73aee44a8cafc46959
Alex Landherr <alex.landherr@gmail.com>
2024-04-03 17:38:11 +0000

    Moved 'to_byte_vector()' & 'byte_vec_to_hex_str()' to pcap.cpp & pcap.h.

--- src/include/pcap.h
+++ src/include/pcap.h
@@ -1,8 +1,13 @@
-
+#include <string>
+#include <vector>
+#include <cstddef>
 
 #ifndef PCAP_H
 #define PCAP_H
 
-namespace pcap {}
+namespace pcap {
+    std::vector<std::byte> to_byte_vector(std::string file_path, unsigned int byte_start_index, unsigned int num_of_bytes);
+    std::string byte_vec_to_hex_str(std::vector<std::byte> &b_vec);
+}
 
 #endif
--- src/pcap.cpp
+++ src/pcap.cpp
@@ -1,3 +1,36 @@
 #include "include/pcap.h"
+#include <string>
+#include <fstream>
+#include <cstdint>
+#include <sstream>
+#include <vector>
+#include <cstddef>
+#include <iomanip>
+#include <bitset>
+#include <iostream>
 
-namespace pcap {}
+namespace pcap {
+    std::vector<std::byte> to_byte_vector(std::string file_path, unsigned int byte_start_index, unsigned int num_of_bytes) {
+        std::vector<std::byte> result(num_of_bytes);
+        std::fstream s{file_path, std::ios::in | std::ios::binary};
+
+        //Check if file can be opened.
+        if (!s.is_open()) {
+            std::cerr << "Failed to open '" << file_path << "'" << '\n';
+        } else {
+            s.seekg(byte_start_index);
+            s.read(reinterpret_cast<char*>(&result.front()), num_of_bytes);
+        }
+
+        return result;
+    }
+
+    std::string byte_vec_to_hex_str(std::vector<std::byte> &b_vec) {
+        std::stringstream ss;
+        for (std::byte b : b_vec) {
+            ss << std::hex << std::setw(2) << std::setfill('0') << std::to_integer<int>(b);
+        }
+
+        return ss.str();
+    }
+}
